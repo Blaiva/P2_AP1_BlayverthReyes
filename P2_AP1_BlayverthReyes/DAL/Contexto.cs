e5621edcf965a3984495315e65b1bd2bf06b94c7
Blaiva <blayverthreyesp14@gmail.com>
2025-10-27 18:43:38 -0400

    Eliminacion de elementos innecesarios y creacion del contexto y modelo

---
+++ P2_AP1_BlayverthReyes/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_BlayverthReyes.Models;
+
+namespace P2_AP1_BlayverthReyes.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions options) : base(options) {}
+
+    public DbSet<Modelos> Modelos { get; set; }
+}
---
+++ P2_AP1_BlayverthReyes/Models/Modelos.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_BlayverthReyes.Models;
+
+public class Modelos
+{
+    [Key]
+    public int Id { get; set; }
+}
